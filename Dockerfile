21fbb905e7cb3a300349ddf4491a131fe9ed09ff
jpowel01 <powell.joshua@epa.gov>
2022-09-09 10:50:53 -0400

    Adjusting automation to allow for less duplicate code when dealing with multiple applications.

--- Dockerfile
+++ Dockerfile
@@ -4,12 +4,14 @@ COPY . /app
 
 WORKDIR /app
 
+ARG TEST_APP
+
+ENV TEST_APP=$TEST_APP
+
 RUN sh /app/karate -e dev --output rapidtox src/rapidtox
 
 FROM httpd:2.4
 
 COPY --from=build /app/rapidtox ./rapidtox
-
-RUN ls -la ./rapidtox
 
 COPY ./rapidtox /usr/local/apache2/htdocs/rapidtox
--- karate-config.js
+++ karate-config.js
@@ -1,5 +1,6 @@
 function() {
     var env = karate.env; // get java system property 'karate.env'
+    var app = java.lang.System.getenv('TEST_APP');
 
     karate.log('karate.env selected environment was:', env);
     karate.configure("ssl", true)
@@ -11,13 +12,13 @@ function() {
   // base config
   var config = {
 	env: env,
-    baseUrl: 'https://ccte-api-rapidtox-dev.epa.gov',
+    baseUrl: `https://ccte-api-${app}-${env}.epa.gov`,
     apiKey: ''
   }
   //switch environment
   if (env == 'dev')
   {
-    config.baseUrl = 'https://ccte-api-rapidtox-dev.epa.gov';
+    config.baseUrl = `https://ccte-api-${app}-${env}.epa.gov`;
     config.apiKey  = 'fake-1ba403ca8938176f3a62de6d30cfb8e';
   }
 	karate.log('baseurl: ',config.baseUrl);
